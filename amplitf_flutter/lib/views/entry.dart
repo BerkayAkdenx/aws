6637a0a8c517017d1b3f425b7d4b3ab3be2c4d7f
İsmet Berkay Akdeniz <74598142+BerkayAkdenx@users.noreply.github.com>
2023-03-16 11:24:08 +0300

    Project is ready for start

---
+++ amplitf_flutter/lib/views/entry.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EntryView extends StatefulWidget {
+  const EntryView({super.key});
+
+  @override
+  State<EntryView> createState() => _EntryViewState();
+}
+
+class _EntryViewState extends State<EntryView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
